a1e85ecf542d4620b553411db5a1f23caac554fd
BarryHennett <harrybennettnz@gmail.com>
2024-03-13 14:21:24 +1300

    Added Pages
    Added customer, supplier and admin pages with links on the home page to each

--- app/src/main/java/com/example/iscg7424_mobileapplication/AdminLogin.java
+++ app/src/main/java/com/example/iscg7424_mobileapplication/AdminLogin.java
@@ -1,8 +1,6 @@
 package com.example.iscg7424_mobileapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,32 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class AdminLogin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_admin_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void GoCusLogin(View view) {
-        Intent intent = new Intent(this, CustomerLogin.class);
-        startActivity(intent);
-    }
-
-    public void GoSupLogin(View view) {
-        Intent intent = new Intent(this, SupplierLogin.class);
-        startActivity(intent);
-    }
-
-    public void GoAdminLogin(View view) {
-        Intent intent = new Intent(this, AdminLogin.class);
-        startActivity(intent);
     }
 }
