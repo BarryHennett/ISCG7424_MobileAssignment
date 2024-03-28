2a8f0ec26a14c671ccff1f47681e9af2e2eb65cc
Harry Bennett <harrybennettnz@gmail.com>
2024-03-28 13:48:13 +1300

    created deal details page

---
+++ app/src/main/java/com/example/iscg7424_mobileapplication/DealDetails.java
@@ -0,0 +1,27 @@
+package com.example.iscg7424_mobileapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class DealDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_deal_details);
+
+    }
+
+    public void gohomepge(View view) {
+        Intent intent = new Intent(this, HomePage.class);
+        startActivity(intent);
+    }
+}
