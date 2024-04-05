23508a6f20a4ff1064d91895071cbf0c3670119a
Harry Bennett <harrybennettnz@gmail.com>
2024-04-05 15:00:54 +1300

    doing back end

--- app/src/main/java/com/example/iscg7424_mobileapplication/CustomerRegister.java
+++ app/src/main/java/com/example/iscg7424_mobileapplication/CustomerRegister.java
@@ -1,6 +1,7 @@
 package com.example.iscg7424_mobileapplication;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +16,10 @@ public class CustomerRegister extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_customer_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+
+    @Override
+    public <T extends View> T findViewById(int id) {
+        return super.findViewById(R.id.FNameCt);
     }
 }
